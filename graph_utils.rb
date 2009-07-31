43faeee8957d1f1ed31aa1d9bd54e51f1b826dd2
Nicholas Faiz <nicholas.faiz@gmail.com>
2009-07-31 18:20:30 +1000

    alpha working version

--- graph_utils.rb
+++ graph_utils.rb
@@ -1,8 +1,12 @@
 module GraphUtils
   
+  def bits_to_date(bits)
+    DateTime.new("20#{bits[2]}".to_i, bits[1].to_i, bits[0].to_i)
+  end
+
   def params_to_date(param_date)
     bits = param_date.split("-")
-    date = DateTime.new("20#{bits[2]}".to_i, bits[1].to_i, bits[0].to_i)
+    date = bits_to_date(bits) 
     return date
   end
 
@@ -32,4 +36,31 @@ module GraphUtils
       
       points
     end
+    
+  #interprets values of d2-1-09=3 into 3 points scored on the 2-1-09 and returns them in an array
+  def gather_point_progress_intervals(params, start_date, end_date, top)
+    days = (end_date - start_date).to_i
+    progress = Array.new(days)
+    
+    dates = {}
+    
+    params.keys.each do |k|
+      if k[0..0] == 'd'
+        bits = k[1..-1].split("-")
+        progressive_day = bits_to_date(bits)
+        dates[progressive_day.to_s] = params[k].to_i
+      end
+    end
+    
+    progress.each_index do |i|
+      day = start_date + i
+      if dates[day.to_s]
+        
+        progress[i] = top - dates[day.to_s]
+        top = top - dates[day.to_s]
+      end
+    end
+    
+    return progress
+  end
 end
--- plane_crash.rb
+++ plane_crash.rb
@@ -13,9 +13,9 @@ end
 
 get "/bd" do
   start_date = params_to_date(params[:start_date])
-  end_date = params_to_date(params[:end_date])
-      
-  total_points = params[:total_points]
+  end_date = params_to_date(params[:end_date])      
+  total_points = params[:tp]
+  progress = gather_point_progress_intervals(params, start_date, end_date, total_points.to_i)
   
   graph = Scruffy::Graph.new(:theme => Scruffy::Themes::Keynote.new)
 
@@ -27,6 +27,8 @@ get "/bd" do
   graph.point_markers = point_markers
   #debugger
   graph.add :line, 'Avg', generate_ideal_points(point_markers.size, total_points.to_i)
+  graph.add :line, 'Actual', progress
+  
   # graph.add :line, 'Avg', [100,75,25,0]
   file_name = "bd_#{Time.now.to_i}.svg"
   graph.render :to => "public/#{file_name}"
